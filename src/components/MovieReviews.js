ca400354255d4f66425541de4f85b8afd9c96b78
Nkosi Enciso Givhan <codeatblack@gmail.com>
2018-11-20 23:24:25 -0800

    finishing up props

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,2 +1,21 @@
 import React, from 'react'
 // Code MovieReviews Here
+
+const MovieReview = (props) => {
+  return (
+    <div className="review">
+      <a href={this.props.link.url}/>
+      {this.props.headline}
+      {this.props.byline}
+      {this.props.summary_short}
+    </div>
+  )
+}
+
+const MovieReviews = ({reviews}) => <div className="review-list">{reviews.map(MovieReview)}</div>
+
+MovieReviews.defaultProps = {
+  reviews: []
+}
+
+export default MovieReviews
